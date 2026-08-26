e25a6009125a51bad8e5bbfb231deb446549a1d2
tvchung <hna.tvchung@gmmail.com>
2026-08-26 08:19:13 +0700

    TvcLesson02

---
+++ TvcLesson02Demo/Controllers/TvcProductController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TvcLesson02Demo.Models;
+
+namespace TvcLesson02Demo.Controllers
+{
+    public class TvcProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            // Đưa dữ liệu ra view
+
+            ViewBag.name = "Chung Trịnh";
+            ViewData["address"] = "Fit NTU ";
+            TempData["UNI"] = "Trường Đại Học Nguyễn Trãi";
+
+            return View();
+        }
+
+        // chi tiết sản  phẩm
+        public IActionResult GetProduct()
+        {
+            //Mock data
+            TvcProduct tvcProduct = new TvcProduct()
+            {
+                ProductId="P001",
+                ProductName="Laptop Dell Vostro",
+                YearRelease=2024,
+                Price=12000000,
+            };
+
+            ViewData["productVD"]=tvcProduct;
+            ViewBag.productVB = tvcProduct;
+
+            return View();
+        }
+    }
+}
---
+++ TvcLesson02Demo/Models/TvcProduct.cs
@@ -0,0 +1,10 @@
+﻿namespace TvcLesson02Demo.Models
+{
+    public class TvcProduct
+    {
+        public string ProductId { get; set; }
+        public string ProductName { get; set; }
+        public int YearRelease { get; set; }
+        public decimal Price { get; set; }
+    }
+}
