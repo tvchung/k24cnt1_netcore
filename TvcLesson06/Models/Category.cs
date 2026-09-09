ba8dfcabb30267e5d1488783829439a59c5d68c4
tvchung <hna.tvchung@gmmail.com>
2026-09-09 08:02:05 +0700

    TvcLesson06

---
+++ TvcLesson06/Models/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace TvcLesson06.Models
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
---
+++ TvcLesson06/ViewComponents/CategoryViewComponent.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TvcLesson06.Models;
+
+namespace TvcLesson06.ViewComponents
+{
+    public class CategoryViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke(int? n)
+        {
+            List<Category> categories = new List<Category>
+            {
+                new Category { CategoryId = 1, CategoryName = "Electronics", IsActive = true },
+                new Category { CategoryId = 2, CategoryName = "Books", IsActive = true },
+                new Category { CategoryId = 3, CategoryName = "Clothing", IsActive = false },
+                new Category { CategoryId = 4, CategoryName = "Home & Kitchen", IsActive = true }
+            };
+
+            n = n ?? 0;
+            var search = categories.Where(c => c.CategoryId > n).ToList();
+            return View(search);
+        }
+    }
+}
